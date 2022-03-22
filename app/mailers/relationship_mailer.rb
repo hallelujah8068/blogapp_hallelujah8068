caa728f6ae4e6414a52f8b965d48349b902d372d
松井晴也 <>
2022-03-22 18:36:54 +0900

    sendgridに関するエラーを修正

--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -15,7 +15,7 @@ class RelationshipMailer < ApplicationMailer
         content = Content.new(type: 'text/plain', value: 'テストメールです')
         mail = Mail.new(from, subject, to, content)
     
-        sg = SendGrid::API.new(api_key: ENV['SENDGRID_WEB_API_KEY'])
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
         response = sg.client.mail._('send').post(request_body: mail.to_json)
       end
     

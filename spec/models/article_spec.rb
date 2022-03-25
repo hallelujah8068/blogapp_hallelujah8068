9615b80ab8d45afdc68b4281e18d427fa2e00f6b
松井晴也 <>
2022-03-26 00:37:39 +0900

    validationによって記事の保存が失敗する場合のテスト

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  context 'タイトルと内容が入力されている場合' do
-    let!(:user) do
-      user = User.create!({
-        email: 'test@example.com',
-        password: 'password'
-      })
-    end
+  let!(:user) do
+    user = User.create!({
+      email: 'test@example.com',
+      password: 'password'
+    })
+  end
 
+  context 'タイトルと内容が入力されている場合' do
     let!(:article) do
       user.articles.build({
         title: Faker::Lorem.characters(number: 10),
@@ -20,5 +20,17 @@ RSpec.describe Article, type: :model do
       expect(article).to be_valid
     end
   end
-  
+
+  context 'タイトルの文字が一文字の場合' do
+    let!(:article) do
+      user.articles.create({
+        title: Faker::Lorem.characters(number: 1),
+        content: Faker::Lorem.characters(number: 300)
+      })
+    end
+
+    it '記事を保存できない' do
+      expect(article.errors.messages[:title][0]).to eq('は2文字以上で入力してください')
+    end
+  end
 end

fc8f81323845191f39fba19040bdcf9ed344749e
松井晴也 <>
2022-03-25 20:04:22 +0900

    moduleを使ってauthenticate_userを設定しているコントローラーのディレクトリを変更

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,19 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
-  resource :timeline, only: [:show]
 
   resources :articles
-  
+
   resources :accounts, only: [:show] do
     resources :follows, only: [:create]
     resources :unfollows, only: [:create]
   end
 
-  resource :profile, only: [:show, :edit, :update]
-  resources :favorites, only: [:index]
+  scope module: :apps do
+    resource :profile, only: [:show, :edit, :update]
+    resources :favorites, only: [:index]
+    resource :timeline, only: [:show]
+  end
 
   namespace :api, defaults: {format: :json} do
     scope '/articles/:article_id' do
